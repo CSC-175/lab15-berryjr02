84cd40ff0472ed3c4afaedd67acd815047acad94
berryjr02 <berryjr@umich.edu>
2025-03-31 21:27:27 -0400

    attempt 1

--- lab15.cpp
+++ lab15.cpp
@@ -1,18 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Function prototypes
-void getInfo(int&, int&);
-double computeWays(int, int);
-double factorial(int);
+void getInfo(int &n, int &k);
+double computeWays(int n, int k);
+double factorial(int num);
 
 /*******************************************************************
 * getLotteryInfo                                                   *
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 ********************************************************************/
+void getInfo(int &n, int &k) {
+    do {
+        cout << "How many balls (1-12) are in the pool to pick from? ";
+        cin >> n;
+        if (cin.fail() || n < 1 || n > 12) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Input error! There must be between 1 and 12 balls." << endl;
+        }
 
+    } while (n < 1 || n > 12);
 
+    do {
+        cout << "How many balls (1-7) will be drawn? ";
+        cin >> k;
+        if (cin.fail() || k < 1 || k > n) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Input error!" << endl;
+        }
+    } while (k < 1 || k > n);
+}
 
 /*******************************************************************
 * computeWays                                                      *
@@ -25,11 +45,18 @@ double factorial(int);
 * multiplying two factorials together. This is done to prevent any *
 * intermediate result becoming so large that it causes overflow.   *
 ********************************************************************/
-
+double computeWays(int n, int k) {
+    double m = (factorial(n)) / (factorial(k) * factorial(n - k));
+    return 1.0 / m;
+}
 
 /*******************************************************************
 * factorial                                                        *
 * This function computes factorials recursively.                   *
 *******************************************************************/
-
+double factorial(int j) {
+    if (j <= 1)
+        return 1;
+    return j * factorial(j - 1);
+}
 
